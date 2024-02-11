425ec1d709cd1f63f42b066f4b126f4795d5fef8
Anastasia <an.korobeynikova@gmail.com>
2024-02-11 15:29:37 +0300

    made read more section

--- src/components/oneProduct/OneProduct.tsx
+++ src/components/oneProduct/OneProduct.tsx
@@ -3,27 +3,64 @@ import iphonebig from '../../assets/iphonebig.svg'
 import starblue_icon from '../../assets/starblue_icon.svg'
 import buy_icon from '../../assets/buy_icon.svg'
 import {useState} from "react";
+
 const OneProduct = () => {
     const [isButtonReadMorePressed, setButtonReadMorePressed] = useState(false)
 
     return (
-        <div className={oneProduct_css.root}>
-            <header className={oneProduct_css.header}>
-                <div className={oneProduct_css.sale}>12.96%</div>
-                <div>off sale</div>
-            </header>
-            <main className={oneProduct_css.productPhoto}>
-                <img src={iphonebig} alt={'product'}/>
-            </main>
-            <footer className={oneProduct_css.info}>
-                <section className={oneProduct_css.idkWhatIsiT}> {/* todo переименовать потом класс*/}
-                    <img src={starblue_icon} alt={'blue star icon'}/>
-                    <div>4.69</div>
+        <div className={isButtonReadMorePressed ? oneProduct_css.rootDescr : oneProduct_css.rootWithoutDescr}>
+            {isButtonReadMorePressed ? null :
+                <section className={oneProduct_css.main}>
+                    <header className={oneProduct_css.header}>
+                        <div className={oneProduct_css.sale}>12.96%</div>
+                        <div>off sale</div>
+                    </header>
+                    <main className={oneProduct_css.productPhoto}>
+                        <img src={iphonebig} alt={'product'}/>
+                    </main>
                 </section>
-                <div>Apple iPhone 9</div>
-                <section className={oneProduct_css.shortInfo}>
-                    <p className={oneProduct_css.aboutProduct}>An apple mobile which is nothing like apple An apple mobile which is nothing like apple An apple mobile which is nothing like apple An apple mobile which is nothing like apple An apple mobile which is nothing like apple An apple mobile which is nothing like apple An apple mobile which is nothing like apple An apple mobile which is nothing like apple An apple mobile which is nothing like apple An apple mobile which is nothing like apple</p>
-                    <button className={oneProduct_css.readMore}>Read more</button>
+            }
+            <footer
+                className={isButtonReadMorePressed ? oneProduct_css.infoWithDescr : oneProduct_css.infoWithoutDescr}>
+                <section className={oneProduct_css.infoWithoutDescr}>
+                    <section className={oneProduct_css.idkWhatIsiT}> {/* todo переименовать потом класс*/}
+                        <img src={starblue_icon} alt={'blue star icon'}/>
+                        <div>4.69</div>
+                    </section>
+                    <div className={oneProduct_css.productName}>Apple iPhone 9</div>
+                    {!isButtonReadMorePressed ?
+                        <section className={oneProduct_css.grid}>
+                            <p className={oneProduct_css.grid1}>
+                                An apple mobile which is nothing like apple An apple
+                            </p>
+                            <p className={oneProduct_css.grid2}>
+                                mobile which is nothing like apple An...
+                            </p>
+                            <button className={oneProduct_css.readMore + ' ' + oneProduct_css.grid3}
+                                    onClick={() => setButtonReadMorePressed(true)}>
+                                Read more
+                            </button>
+                        </section>
+                        :
+                        <section className={oneProduct_css.shortInfo}>
+                            <p className={oneProduct_css.aboutProductFull}>
+                                An apple mobile which is nothing like apple An apple mobile which is nothing like apple
+                                An apple
+                                mobile which is nothing like apple An apple mobile which is nothing like apple An apple
+                                mobile
+                                which is nothing like apple An apple mobile which is nothing like apple An apple mobile
+                                which is
+                                nothing like apple An apple mobile which is nothing like apple An apple mobile which is
+                                nothing
+                                like apple An apple mobile which is nothing like apple
+                            </p>
+                            <button className={oneProduct_css.readMore}
+                                    onClick={() => setButtonReadMorePressed(false)}>
+                                Hide description
+                            </button>
+                        </section>
+                    }
+
                 </section>
                 <section className={oneProduct_css.price}>
                     <button className={oneProduct_css.buttonAddToCart}>
